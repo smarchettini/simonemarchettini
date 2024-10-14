4e9c452535cbf334824333d7a56c21e58a874133
Simone Marchettini <simonemarchettini@gmail.com>
2024-10-14 21:41:59 +0200

    Update Seconda_Bozza.py

--- Seconda_Bozza.py
+++ Seconda_Bozza.py
@@ -5,20 +5,18 @@ import time
 # Funzione per mostrare i messaggi con ritardo
 def mostra_messaggi_con_ritardo():
     # Riserva uno spazio per aggiornare i messaggi
+    time.sleep(4)
     messaggio_1 = st.empty()
-    messaggio_2 = st.empty()
 
     # Mostra il primo messaggio e aspetta 4 secondi
     messaggio_1.write("1️⃣ Vuoi scoprire cosa il destino ha in serbo per te? Fai una domanda sul futuro!")
     time.sleep(4)
 
     # Mostra il secondo messaggio e aspetta 4 secondi
-    messaggio_2.write("2️⃣ Vorresti conoscere meglio Simone e le sue capacità lavorative? Fai una domanda su di lui!")
-    time.sleep(4)
-
+    messaggio_1.write("2️⃣ Vorresti conoscere meglio Simone e le sue capacità lavorative? Fai una domanda su di lui!")
+    
 # Titolo dell'app
 st.title("Interazione sequenziale in Streamlit")
 
 # Bottone per avviare l'interazione
-if st.button("Inizia"):
-    mostra_messaggi_con_ritardo()
+mostra_messaggi_con_ritardo()
