3895459383dbab47ed8886f59466b9115dbf19e2
Simone Marchettini <simonemarchettini@gmail.com>
2024-10-14 20:53:13 +0200

    Update Bozza.py

--- Bozza.py
+++ Bozza.py
@@ -5,14 +5,13 @@ import time
 def mostra_messaggi_con_ritardo():
     # Riserva uno spazio per aggiornare i messaggi
     messaggio_1 = st.empty()
-    messaggio_2 = st.empty()
 
     # Mostra il primo messaggio e aspetta 4 secondi
     messaggio_1.write("1️⃣ Vuoi scoprire cosa il destino ha in serbo per te? Fai una domanda sul futuro!")
     time.sleep(4)
 
     # Mostra il secondo messaggio e aspetta 4 secondi
-    messaggio_2.write("2️⃣ Vorresti conoscere meglio Simone e le sue capacità lavorative? Fai una domanda su di lui!")
+    messaggio_1.write("2️⃣ Vorresti conoscere meglio Simone e le sue capacità lavorative? Fai una domanda su di lui!")
     time.sleep(4)
 
 # Titolo dell'app
