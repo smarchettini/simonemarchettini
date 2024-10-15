af097b4f87c33d3687eb6988fc9a52148a5c7ec9
Simone Marchettini <simonemarchettini@gmail.com>
2024-10-15 12:01:13 +0200

    Update Prima_Bozza.py

--- Prima_Bozza.py
+++ Prima_Bozza.py
@@ -132,7 +132,7 @@ def main():
 
     else:
         # Se il gioco è chiuso, mostra solo il messaggio di ringraziamento
-        st.write("🎉 Grazie per aver giocato! A presto! 👋")
+        st.write(" Grazie per aver giocato! 🎉 A presto! 👋")
 
 if __name__ == "__main__":
     main()
